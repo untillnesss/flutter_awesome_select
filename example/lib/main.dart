6f4f244c1e2c97aa187165a9f8d6c381f50b7d17
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2021-09-23 04:46:41 +0700

    new simple example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,41 +1,152 @@
+// import 'package:flutter/material.dart';
+// import 'package:theme_patrol/theme_patrol.dart';
+// import 'features.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return ThemePatrol(
+//       light: ThemeData(
+//         brightness: Brightness.light,
+//         primarySwatch: Colors.red,
+//         primaryColor: Colors.red,
+//         accentColor: Colors.red,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//         useTextSelectionTheme: true,
+//       ),
+//       dark: ThemeData(
+//         brightness: Brightness.dark,
+//         primarySwatch: Colors.red,
+//         primaryColor: Colors.red,
+//         accentColor: Colors.red,
+//         toggleableActiveColor: Colors.red,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//         useTextSelectionTheme: true,
+//       ),
+//       mode: ThemeMode.system,
+//       builder: (context, theme) {
+//         return MaterialApp(
+//           title: 'Smart Select',
+//           theme: theme.light,
+//           darkTheme: theme.dark,
+//           themeMode: theme.mode,
+//           home: Features(),
+//         );
+//       },
+//     );
+//   }
+// }
+
+import 'package:awesome_select/awesome_select.dart';
+import 'package:awesomeselect/select.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:theme_patrol/theme_patrol.dart';
-import 'features.dart';
 
-void main() => runApp(MyApp());
+main(List<String> args) {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return ThemePatrol(
-      light: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.red,
-        primaryColor: Colors.red,
-        accentColor: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        useTextSelectionTheme: true,
-      ),
-      dark: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.red,
-        primaryColor: Colors.red,
-        accentColor: Colors.red,
-        toggleableActiveColor: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        useTextSelectionTheme: true,
-      ),
-      mode: ThemeMode.system,
-      builder: (context, theme) {
-        return MaterialApp(
-          title: 'Smart Select',
-          theme: theme.light,
-          darkTheme: theme.dark,
-          themeMode: theme.mode,
-          home: Features(),
-        );
-      },
+    return MaterialApp(
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 12),
+            HondaSelectOutline<int>(
+              title: 'Pilih Produk',
+              choiceItems: this.list,
+              onChange: this.onChangeProduct,
+              selectedValue: this.selectedProduct,
+              // isLoading: _.isLoadingProduct,
+            ),
+            SmartSelect<int>.single(
+              title: 'Jajaln',
+              selectedValue: this.selectedProduct,
+              choiceItems: this.list,
+              onChange: (state) => this.onChangeProduct(state, state.value),
+              modalFilter: true,
+              modalFilterAuto: true,
+              modalHeaderStyle: S2ModalHeaderStyle(
+                backgroundColor: Colors.white,
+                actionsIconTheme: IconThemeData(color: Colors.red),
+                iconTheme: IconThemeData(color: Colors.black54),
+                textStyle: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: this.gantiList,
+                  child: Text('Ganti list'),
+                ),
+                ElevatedButton(
+                  onPressed: this.setValue,
+                  child: Text('Set value'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  int selectedProduct;
+
+  onChangeProduct(state, value) {
+    this.selectedProduct = value;
+    setState(() {});
+  }
+
+  List<S2Choice<int>> list = [
+    S2Choice(value: 2, title: 'Dua'),
+  ];
+
+  gantiList() {
+    setState(() {
+      this.selectedProduct = null;
+      this.list = [
+        S2Choice(value: 1, title: 'Satu'),
+        S2Choice(value: 2, title: 'Dua'),
+      ];
+    });
+  }
+
+  setValue(){
+    setState(() {
+      this.selectedProduct = 1;
+    });
   }
 }
---
+++ example/lib/select.dart
@@ -0,0 +1,77 @@
+import 'package:awesome_select/awesome_select.dart';
+import 'package:flutter/material.dart';
+
+class HondaSelectOutline<T> extends StatefulWidget {
+  final String title;
+  final bool isLoading;
+  final List<S2Choice<T>> choiceItems;
+  final Function(dynamic state, T value) onChange;
+  final T selectedValue;
+  final EdgeInsets margin;
+
+  const HondaSelectOutline({
+    Key key,
+    this.title,
+    this.isLoading = false,
+    this.choiceItems,
+    this.onChange,
+    this.selectedValue,
+    this.margin,
+  }) : super(key: key);
+
+  @override
+  _HondaSelectOutlineState<T> createState() => _HondaSelectOutlineState<T>();
+}
+
+class _HondaSelectOutlineState<T> extends State<HondaSelectOutline<T>> {
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        margin: this.widget.margin ??
+            EdgeInsets.symmetric(horizontal: 12).copyWith(bottom: 12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.grey),
+        ),
+        child: SmartSelect<T>.single(
+          title: this.widget.title,
+          selectedValue: this.widget.selectedValue,
+          choiceItems: this.widget.choiceItems,
+          onChange: (state) => this.widget.onChange(state, state.value),
+          modalFilter: true,
+          modalFilterAuto: true,
+          modalHeaderStyle: S2ModalHeaderStyle(
+            backgroundColor: Colors.white,
+            actionsIconTheme: IconThemeData(color: Colors.red),
+            iconTheme: IconThemeData(color: Colors.black54),
+            textStyle: TextStyle(
+              color: Colors.black54,
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
+            ),
+          ),
+          tileBuilder: (context, state) {
+            return S2Tile.fromState(state,
+                dense: true,
+                isLoading: this.widget.isLoading,
+                title: state.selected.value == null
+                    ? Text(
+                        state.title,
+                        style: TextStyle(color: Colors.black38, fontSize: 16),
+                      )
+                    : Text(
+                        state.selected?.title?.toString() ?? this.widget.title,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                padding: EdgeInsets.symmetric(horizontal: 14),
+                hideValue: true,
+                trailing: Icon(Icons.arrow_downward));
+          },
+        ),
+      ),
+    );
+  }
+}
